bc7216d30c4d4bfb880d8f668a10d9567ada47a5
victorm0612 <rojasvictorvargas@gmail.com>
2021-06-29 19:32:13 -0500

    fixed fronted card

--- BackEnd/api/src/index.js
+++ BackEnd/api/src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cron = require('node-cron');
 const fetch = require('node-fetch');
+const cors = require('cors');
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
 
 var app = express();
 app.use(express.json())
+app.use(express.cors());
 
 const PORT = 8000;
 
@@ -10,47 +15,27 @@ app.listen(PORT, function(){
   console.log(`Server running at port ${PORT}`);
 });
 
-app.post('/test_user', function(req, res){
-  const {site_id} = req.body;
-  fetch("https://api.mercadolibre.com/users/test_user", {
-    body: {"site_id": site_id},
-    headers: {
-      Authorization: "Bearer $ACCESS_TOKEN",
-      "Content-Type": "application/json"
-    },
-    method: "POST"
-  })
-})
-
-app.post('/auth', function(req, res){
-  const {client_id, client_secret, redirect_uri} = req.body;
-  fetch("https://api.mercadolibre.com/oauth/token", {
-    body: `grant_type=authorization_code&client_id=$${client_id}&client_secret=$${client_secret}&code=$code&redirect_uri=$${redirect_uri}`,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    method: "POST"
-  })
-  .then(response => {
-    console.log("Request data:");
-    console.log(req);
-  })
-  .catch(error => {
-    res.status(500).json({
-      message: `El error es: ${error}`
-    })
-  })
-})
-
-app.get('/', function(req, res){
-  const {q,site_id,token} = req.body;
-  axios.get(`https://api.mercadolibre.com/sites/${site_id}/search?q=${q}`, {
-  headers: {
-    Authorization: `Bearer ${token}`
+//routes
+app.get('/', (req, res) => {
+  const {q, access_token} = req.body;
+  fetch(`https://api.mercadolibre.com/sites/MCO/search?q=${q}&sort=price_asc`,
+    {
+      headers: {
+        Authorization: `Bearer $${access_token}`,
+      },
     }
-  })
-  .then(response => {
-    console.log(response);
-  })
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data =>{
+      res.json({
+        "info": data.results
+      }) 
+    })
 })
+
+
+cron.schedule('*/1 * * * *', () => {
+  console.log('running a task every minute');
+});
---
+++ FrontEnd/functions.js
@@ -0,0 +1,146 @@
+var client_id = "880976774391862";
+var client_secret = "qCsOgLKXPHnieNz0K5ROACIQZs7wH1qC";
+var redirect_uri = "https://mercadolb.herokuapp.com/";
+var code, q, total=0;
+var list = [];
+
+var user = JSON.parse(localStorage.getItem("dataUser")) || {
+  id_user: null,
+  token: null,
+};
+
+if (user.access_token || user.user_id) {
+  document.querySelector(".code").className =
+    "invisible code row p-5 justify-content-center text-center";
+  document.querySelector(".search").className =
+    "visible search row p-5 justify-content-center text-center";
+}
+
+document.querySelector(".login").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.open(
+    `http://auth.mercadolibre.com.co/authorization?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`,
+    "_blank"
+  );
+});
+
+document.querySelector(".sendCode").addEventListener("click", async (e) => {
+    e.preventDefault();
+    code = document.getElementById("codigo").value;
+    document.querySelector(".code").className =
+      "invisible code row p-5 justify-content-center text-center";
+
+    await fetch("https://api.mercadolibre.com/oauth/token", {
+      body: `grant_type=authorization_code&client_id=${client_id}&client_secret=${client_secret}&code=${code}&redirect_uri=${redirect_uri}`,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem(
+          "dataUser",
+          JSON.stringify({
+            access_token: data.access_token,
+            user_id: data.user_id,
+          })
+        );
+        document.querySelector(".search").className =
+          "visible search row p-5 justify-content-center text-center";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+document.querySelector(".sendQ").addEventListener("click", async (e) => {
+  e.preventDefault();
+  q = document.getElementById("busqueda").value;
+  await fetch(
+    `https://api.mercadolibre.com/sites/MCO/search?q=${q}&sort=price_asc`,
+    {
+      headers: {
+        Authorization: `Bearer $${user.access_token}`,
+      },
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.results.filter((e) => {
+        best_seller(e.seller.id, e);
+      });
+      document.querySelector('.buttonTable').className = 'buttonTable visible text-center'
+    });
+});
+
+async function best_seller(seller_id, element) {
+  await fetch(`https://api.mercadolibre.com/users/${seller_id}`, {
+    headers: {
+      Authorization: `Bearer $${user.access_token}`,
+    },
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    if (data.seller_reputation.level_id == "5_green") {
+      list.push(element);
+    }
+  });
+}
+
+document.querySelector('.show').addEventListener('click', (e)=>{
+  e.preventDefault();
+  orderByPrice(list);
+  paintTable(list);
+})
+
+function orderByPrice(array){
+  array.sort(function (a, b){
+    if (a.price < b.price)
+      return -1;
+    if ( a.price > b.price )
+      return 1;
+    return 0;
+  })
+}
+
+function paintTable(array){
+  document.querySelector('#cuerpo').innerHTML =`
+  <table class="table table-light">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Titulo</th>
+        <th scope="col">Precio</th>
+        <th scope="col">Compra</th>
+      </tr>
+    </thead>
+    <tbody id="Incuerpo">
+    </tbody>
+  </table> 
+  `
+  for(let element of array){
+    document.querySelector('#Incuerpo').innerHTML +=`
+        <tr>
+          <th scope="row">${element.id}</th>
+          <td>${element.title}</td>
+          <td>$${new Intl.NumberFormat().format(element.price)}</td>
+          <td><a class="btn btn-primary" target="_blank" href="${element.permalink}">Comprar</td>
+        </tr>
+    `
+    total +=parseInt(element.price);
+  }
+  total = Math.floor(total/array.length);
+  fetch(`https://api.thingspeak.com/update?api_key=7GJS9PZ8CQVG3DMT&field1=${total}`,{
+  method: 'GET'
+  })
+  .then(response => {return response})
+}
